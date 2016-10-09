1788c4ddd3cfbe75760b513c276323e09a73c237
MyNESCreations <tyhbrid@gmail.com>
2016-10-09 19:45:19 -0400

    Updated Slider

--- Assets/Scripts/uiStatsManager.cs
+++ Assets/Scripts/uiStatsManager.cs
@@ -1,6 +1,26 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class uiStatsManager : MonoBehaviour {
 
+    [SerializeField]
+    Slider mindPowerSlider;
+
+    //[HideInInspector]
+    public float currentMindPower;
+    public float maxMindPower;
+
+    void Start()
+    {
+        mindPowerSlider.maxValue = maxMindPower;
+        mindPowerSlider.minValue = 0;
+        currentMindPower = maxMindPower;
+        mindPowerSlider.value = maxMindPower;
+    }
+
+    void Update()
+    {
+        mindPowerSlider.value = currentMindPower;
+    }
 }
