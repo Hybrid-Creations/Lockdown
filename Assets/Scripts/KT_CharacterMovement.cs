97391029173ccad1ad3a94f9e5ada21df1b3b3fd
DESKTOP-PRMUGSK\Kevin Thomas <kmthomas1224@gmail.com>
2016-10-09 20:12:58 -0400

    Player Movement
    Added character movement
    Added box colliders to characters
    Remade player & redlab prefabs
    Added box colliders to wall frames

---
+++ Assets/Scripts/KT_CharacterMovement.cs
@@ -0,0 +1,46 @@
+﻿//Name: Kevin Thomas
+//Date: 10/9/16
+//Credit:
+//
+//Purpose: To create movement for characters
+
+using UnityEngine;
+using System.Collections;
+
+public class KT_CharacterMovement : MonoBehaviour {
+
+    [SerializeField]
+    float moveSpeed = 3;
+
+    public GameObject currentControl;
+
+	// Use this for initialization
+	void Start () {
+        currentControl = GameObject.FindGameObjectWithTag("Player");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        CharacterMovement();
+	}
+
+    void CharacterMovement()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            currentControl.transform.position += currentControl.transform.up * moveSpeed;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            currentControl.transform.position += -currentControl.transform.right * moveSpeed;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            currentControl.transform.position += currentControl.transform.right * moveSpeed;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            currentControl.transform.position += -currentControl.transform.up * moveSpeed;
+        }
+    }
+}
