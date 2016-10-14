0c512edf015365f766f459b2a257a7005c07fb7d
DESKTOP-PRMUGSK\Kevin Thomas <kmthomas1224@gmail.com>
2016-10-14 16:23:59 -0400

    Added Script
    Script and trigger for InCell are added
    Static bool inCell now accessible

---
+++ Assets/Scripts/KT_InCell.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KT_InCell : MonoBehaviour {
+
+    public static bool inCell;
+
+    void OnTriggerStay2D(Collider2D other)
+    {
+        if(other.tag == "Player")
+        {
+            inCell = true;
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if(other.tag == "Player")
+        {
+            inCell = false;
+        }
+    }
+}
