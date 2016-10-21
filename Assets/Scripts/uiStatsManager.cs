e958885d224bca8ececfe6e9da9c32895dcd4808
MyNESCreations <tyhbrid@gmail.com>
2016-10-20 21:49:31 -0400

    Fixed MP being at 0 max

--- Assets/Scripts/uiStatsManager.cs
+++ Assets/Scripts/uiStatsManager.cs
@@ -16,7 +16,7 @@ public class uiStatsManager : MonoBehaviour
     Text valueText;
     //[HideInInspector]
     public float currentMindPower;
-    public static float maxMindPower;
+    public static float maxMindPower = 100;
 
     [Header("Caught Values")]
     [SerializeField]
