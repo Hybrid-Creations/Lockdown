e515e1a48962a5fd51bb415daecfcae3c192bf2d
DESKTOP-PRMUGSK\Kevin Thomas <kmthomas1224@gmail.com>
2016-10-23 16:43:59 -0400

    Added audio
    Fixed text in opening

---
+++ Assets/Scripts/KT_AudioManager.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KT_AudioManager : MonoBehaviour {
+
+    [SerializeField]
+    AudioSource audManager;
+    
+    void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+
+	// Use this for initialization
+	void Start () {
+        audManager = GetComponent<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
