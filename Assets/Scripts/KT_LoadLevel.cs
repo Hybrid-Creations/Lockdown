9a207b60653b020ccc4acd50cc1c8f7231858c93
MyNESCreations <tyhbrid@gmail.com>
2016-10-23 16:00:41 -0400

    now limits maxPower

--- Assets/Scripts/KT_LoadLevel.cs
+++ Assets/Scripts/KT_LoadLevel.cs
@@ -6,6 +6,22 @@ public class KT_LoadLevel : MonoBehaviour
 {
     void OnTriggerEnter2D (Collider2D other)
     {
+        if (SceneManager.GetActiveScene().buildIndex == 3)
+        {
+            if (uiStatsManager.maxMindPower > 110)
+                uiStatsManager.maxMindPower = 110;
+        }
+        if (SceneManager.GetActiveScene().buildIndex == 4)
+        {
+            if (uiStatsManager.maxMindPower > 120)
+                uiStatsManager.maxMindPower = 120;
+        }
+        if (SceneManager.GetActiveScene().buildIndex == 5)
+        {
+            if (uiStatsManager.maxMindPower > 130)
+                uiStatsManager.maxMindPower = 130;
+        }
+
         if (other.tag == "Player")
             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
